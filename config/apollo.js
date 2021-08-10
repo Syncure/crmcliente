9862c2f2ff386d0f32b8a8b6b3ff38d3c741f513
Syncure <richvargast@gmail.com>
2021-08-10 16:28:55 -0500

    commit 4

--- config/apollo.js
+++ config/apollo.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'https://afternoon-tor-05688.herokuapp.com',
+    uri: 'https://fast-plateau-24355.herokuapp.com/',
     fetch
 })
 // https://afternoon-tor-05688.herokuapp.com
