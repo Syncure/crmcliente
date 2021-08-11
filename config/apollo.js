76d1f648a247670fcaf20729a236f360f42cc003
Richard Kevin Vargas Torres <richvargast@gmail.com>
2021-08-10 20:16:21 -0500

    Update apollo.js

--- config/apollo.js
+++ config/apollo.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:4000/',     
+    uri: 'https://murmuring-island-97391.herokuapp.com/',     
     fetch
 })
 // https://afternoon-tor-05688.herokuapp.com
