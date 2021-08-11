4004eba586ab2a45adf0d752b9cec54a73bd4ae3
Richard Kevin Vargas Torres <richvargast@gmail.com>
2021-08-10 19:03:43 -0500

    Update apollo.js

--- config/apollo.js
+++ config/apollo.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'https://mighty-springs-18720.herokuapp.com/',     
+    uri: 'https://afternoon-cliffs-28965.herokuapp.com/',     
     fetch
 })
 // https://afternoon-tor-05688.herokuapp.com
